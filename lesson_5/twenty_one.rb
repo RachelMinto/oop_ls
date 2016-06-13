a523320127394c4e8bc8e6f5b5b4851e66127fa4
Rachel Minto <rachel.j.minto@gmail.com>
2016-06-13 17:05:47 +0000

    Working through spike.

--- lesson_5/twenty_one.rb
+++ lesson_5/twenty_one.rb
@@ -1,26 +1,25 @@
-require 'pry'
 
 class Participant
-  attr_accessor :hand
+  attr_accessor :hand, :total
   def initialize
     @total = 0
     @hand = []
   end
 
-  def hit
-
-  end
-
   def stay
-
+    puts "You have chosen to stay with a total of #{total}."
   end
 
   def busted?
 
   end
-
-  def total
-
+  
+  def show_cards(show_all_cards=true)
+    if show_all_cards
+      hand.each { |card| puts "#{card.value} of #{card.suit}" }
+    else
+      puts "#{hand.first.value} of #{hand.first.suit} and one unknown card."
+    end
   end
 end
 
@@ -28,12 +27,20 @@ class Player < Participant
   def initialize
     super
   end
+  
 end
 
 class Dealer < Participant
+  attr_accessor :deck
+  
   def initialize(deck)
     super()
     @deck = deck
+  end
+  
+  def hit(participant)
+    card = deal(participant)
+    puts "You have chosen to hit and have been dealt the #{card.value} of #{card.suit}."
   end
 
   def stay
@@ -42,9 +49,9 @@ class Dealer < Participant
 
   def deal(player)
     card = @deck.cards.sample
-    @deck.dealt_cards.push(card)
+    deck.dealt_cards.push(card)
     player.hand.push(card)
-    puts "I've dealt a #{card.value} of #{card.suit} to #{player}"
+    return card
   end
 end
 
@@ -87,6 +94,7 @@ end
 
 class Game
   include Display
+  attr_accessor :player, :dealer, :deck
 
   def initialize
     @deck = Deck.new
@@ -97,17 +105,37 @@ class Game
   def play
     display_welcome_message
     deal_cards
-    # display_initial_cards
-    # player_turn
+    display_initial_cards
+    player_turn
     # dealer_turn
     # display_result
   end
 
   def deal_cards
-    @dealer.deal(@player)
-    @dealer.deal(@player)
-    @dealer.deal(@dealer)
-    @dealer.deal(@dealer)
+    dealer.deal(player)
+    dealer.deal(player)
+    dealer.deal(dealer)
+    dealer.deal(dealer)
+  end
+  
+  def display_initial_cards
+    player.show_cards(true)
+    dealer.show_cards(false)
+  end
+  
+  def player_turn
+    loop do
+      answer = ''
+      unless player.busted?
+        loop do
+          puts "Would you like to hit or stay?"
+          answer = gets.chomp.downcase
+          break if %w(hit stay h s).include? answer
+          puts "I'm sorry, you must reply with either hit or stay."
+        end
+        answer.start_with?('h') ? dealer.hit(player) : player.stay
+      end
+    end
   end
 end
 
