2e3cd8f44298ddf377a8f5a50465906c98ffda1a
RachelMinto <rachel.j.minto@gmail.com>
2016-06-15 07:13:10 -0400

    Working cards display.

--- lesson_5/twenty_one.rb
+++ lesson_5/twenty_one.rb
@@ -1,25 +1,76 @@
 
+# frozen_string_literal: true
+
+# To do: set up nice playing screen. Clear. List player cards. Colorize.
+# Don't clear until pressing enter after welcome. 
+# Don't let dealer hit if it busts.
+
+require 'pry'
+
 class Participant
-  attr_accessor :hand, :total
+  MAX_ALLOWED_POINTS = 21
+  attr_accessor :hand
+  attr_reader :name
+
   def initialize
     @total = 0
     @hand = []
-  end
-
-  def stay
-    puts "You have chosen to stay with a total of #{total}."
+    @turn_complete = false
+    set_name
   end
 
   def busted?
-
+    @total > MAX_ALLOWED_POINTS
   end
-  
+
+  def stayed?
+    @turn_complete
+  end
+
+  def stay
+    puts "#{name} has chosen to stay with a total of #{total} points."
+    @turn_complete = true
+  end
+
+  def cards_in_array
+    cards = []
+    hand.each { |card| cards << [card.value, card.suit] }
+    cards
+  end
+
   def show_cards(show_all_cards=true)
+    described_cards = []
+    cards = cards_in_array
     if show_all_cards
-      hand.each { |card| puts "#{card.value} of #{card.suit}" }
+      cards.each { |value, suit| described_cards << "#{value} of #{suit}" }
     else
-      puts "#{hand.first.value} of #{hand.first.suit} and one unknown card."
+      described_cards.push("#{cards[0][0]} of #{cards[0][1]}")
+      described_cards.push("Hidden card")
     end
+    described_cards
+  end
+
+  def show_total
+    puts "#{name} has a total of #{total} points."
+  end
+
+  def total
+    sum = 0
+
+    hand.each do |card|
+      sum += if card.value == "Ace"
+               11
+             elsif card.value.to_i == 0
+               10
+             else
+               card.value.to_i
+             end
+    end
+
+    hand.each.select { |card| card.value == "Ace" }.count.times do
+      sum -= 10 if sum > MAX_ALLOWED_POINTS
+    end
+    sum
   end
 end
 
@@ -27,38 +78,51 @@ class Player < Participant
   def initialize
     super
   end
-  
+
+  def set_name
+    system('clear') || system('cls')
+    name = ''
+    puts ''
+    puts "Hello, what is your name?"
+    loop do
+      name = gets.chomp
+      break unless name.to_s.strip.empty? || name.to_s.length > 17
+      puts "Sorry, your name must be between 1 and 17 characters long."
+    end
+    @name = name
+  end
 end
 
 class Dealer < Participant
   attr_accessor :deck
-  
+
   def initialize(deck)
     super()
     @deck = deck
   end
-  
-  def hit(participant)
-    card = deal(participant)
-    puts "You have chosen to hit and have been dealt the #{card.value} of #{card.suit}."
+
+  def set_name
+    @name = ['Jane Austen', 'George Eliot', 'Charles Dickens'].sample
   end
 
-  def stay
-
+  def hit(participant)
+    card = deal(participant)
+    puts "#{participant.name} has been dealt the #{card.value} of #{card.suit}."
   end
 
   def deal(player)
     card = @deck.cards.sample
     deck.dealt_cards.push(card)
     player.hand.push(card)
-    return card
+    card
   end
 end
 
 class Deck
   attr_accessor :cards, :dealt_cards
-  SUITS = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-  VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+  SUITS = ['Hearts', 'Diamonds', 'Spades', 'Clubs'].freeze
+  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10'] +
+           ['Jack', 'Queen', 'King', 'Ace']
 
   def initialize
     cards = []
@@ -88,13 +152,76 @@ end
 
 module Display
   def display_welcome_message
-    puts "Welcome to Twenty-One!"
+    clear
+    puts "Welcome to Twenty-One, #{player.name}!"
+    puts "#{dealer.name} will be your dealer today."
+    puts ""
+    sleep(1.8)
+  end
+
+  # def display_initial_cards
+  #   player.show_cards(true)
+  #   dealer.show_cards(false)
+  #   puts "please press Enter to continue."
+  #   gets.chomp
+  # end
+
+  def display_game_state
+    clear
+    width = Game::LINE_WIDTH
+    display_hands(width)
+  end
+
+  def display_hands(width)
+    display_player_titles(width)
+    display_player_cards(width)
+    puts ""
+    puts "*  *  *  *  *  *  *".center(width)
+    puts ""
+  end
+
+  def display_player_titles(width)
+    dealer_title = "#{dealer.name}'s cards:"
+    player_title = "#{player.name}'s cards:"
+    puts player_title.ljust(width / 2) + dealer_title.rjust(width / 2)
+  end
+
+  def display_player_cards(width)
+    player_cards = player.cards_in_array
+    dealer_cards = dealer.cards_in_array
+    shortest_length = player_cards.length < dealer_cards.length ? player_cards.length : dealer_cards.length
+    i = 0
+    while i < shortest_length
+      puts player.show_cards[i].ljust(width / 2) + dealer.show_cards(false)[i].rjust(width / 2)
+      i += 1
+    end
+    if player_cards.length > shortest_length
+      puts player.show_cards[shortest_length..player_cards.length]
+    elsif dealer_cards.length > shortest_length
+      puts dealer.show_cards[shortest_length..player_cards.length]
+    end
+  end
+
+  def display_result
+    puts ""
+    if dealer.busted?
+      puts "The dealer has busted so you win!"
+    elsif player.busted?
+      puts "You have busted so the dealer wins!"
+    else
+      puts "Your total is #{player.total} and the dealer has #{dealer.total}."
+    end
+  end
+
+  def clear
+    system('clear') || system('cls')
   end
 end
 
 class Game
   include Display
   attr_accessor :player, :dealer, :deck
+  LINE_WIDTH = 64
 
   def initialize
     @deck = Deck.new
@@ -105,10 +232,10 @@ class Game
   def play
     display_welcome_message
     deal_cards
-    display_initial_cards
+    # display_initial_cards
     player_turn
-    # dealer_turn
-    # display_result
+    dealer_turn
+    display_result
   end
 
   def deal_cards
@@ -117,24 +244,41 @@ class Game
     dealer.deal(dealer)
     dealer.deal(dealer)
   end
-  
-  def display_initial_cards
-    player.show_cards(true)
-    dealer.show_cards(false)
-  end
-  
+
   def player_turn
     loop do
-      answer = ''
-      unless player.busted?
-        loop do
-          puts "Would you like to hit or stay?"
-          answer = gets.chomp.downcase
-          break if %w(hit stay h s).include? answer
-          puts "I'm sorry, you must reply with either hit or stay."
-        end
-        answer.start_with?('h') ? dealer.hit(player) : player.stay
+      display_game_state
+      break if player.busted? || player.stayed?
+      answer = hit_or_stay
+      case answer
+      when /^h/ then dealer.hit(player)
+      when /^s/ then player.stay
+      when /^t/ then puts "Your cards total to #{player.total} points."
+        #Need to prevent from clearing before being seen.
       end
+    end
+  end
+
+  def hit_or_stay
+    puts ""
+    answer = ''
+    loop do
+      puts "Would you like to hit, stay or view your current total? (h/s/t)"
+      answer = gets.chomp.downcase
+      break if %w(hit stay total h s t).include? answer
+      puts "I'm sorry, you must reply with either hit, stay or total."
+    end
+    answer
+  end
+
+  def dealer_turn
+    unless player.busted?
+      while dealer.total < 17 || dealer.total < player.total || dealer.busted?
+        dealer.show_cards(true)
+        dealer.hit(dealer)
+        break if dealer.busted?
+      end
+      dealer.stay unless dealer.busted?
     end
   end
 end
